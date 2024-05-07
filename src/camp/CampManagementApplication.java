32851b883d6ffca2d8893015931170d2f5e07b97
geon <lumineno@naver.com>
2024-05-07 22:13:10 +0900

    점수등록

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -5,6 +5,7 @@ import camp.model.Student;
 import camp.model.Subject;
 
 import java.util.*;
+//import java.util.stream.Collectors;
 
 /**
  * Notification
@@ -24,7 +25,7 @@ public class CampManagementApplication {
     private static String SUBJECT_TYPE_MANDATORY = "MANDATORY";
     private static String SUBJECT_TYPE_CHOICE = "CHOICE";
 
-//    private static Map<String,String> subjectMap = new HashMap<>();
+    private static Map<String,String> subjectMap = new HashMap<>();
     // index 관리 필드
     private static int studentIndex;
     private static final String INDEX_TYPE_STUDENT = "ST";
@@ -38,9 +39,9 @@ public class CampManagementApplication {
 
     public static void main(String[] args) {
         setInitData();
-//        for (Map.Entry<String, String> entry : subjectMap.entrySet()) {
-//            System.out.println("[Key]:" + entry.getKey() + " [Value]:" + entry.getValue());
-//        }
+        for (Map.Entry<String, String> entry : subjectMap.entrySet()) {
+            System.out.println("[Key]:" + entry.getKey() + " [Value]:" + entry.getValue());
+        }
         try {
             displayMainView();
 
@@ -52,15 +53,15 @@ public class CampManagementApplication {
     // 초기 데이터 생성
     private static void setInitData() {
         studentStore = new ArrayList<>();
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Java");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"객체지향");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Spring");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"JPA");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"MySQL");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"디자인 패턴");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Spring Security");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Redis");
-//        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"MongoDB");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Java");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"객체지향");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Spring");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"JPA");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"MySQL");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"디자인 패턴");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Spring Security");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"Redis");
+        subjectMap.put(sequence(INDEX_TYPE_SUBJECT),"MongoDB");
 
         subjectStore = List.of(
                 new Subject(
@@ -110,7 +111,6 @@ public class CampManagementApplication {
                 )
         );
         scoreStore = new ArrayList<>();
-
     }
 
     // index 자동 증가
@@ -211,6 +211,7 @@ public class CampManagementApplication {
             int subjectNumber = Integer.parseInt(subject)-1;
             studentsubjectList.add(subjectStore.get(subjectNumber).getSubjectName());
         });
+
         do {
             System.out.println("수강생의 선택과목 목록 중 2가지 선택해주세요. [1.디자인_패턴 2.Spring_Security 3.Redis 4.MongoDB]");
             studentSubject = sc.nextLine();
@@ -259,9 +260,9 @@ public class CampManagementApplication {
     private static void inquireStudent() {
         System.out.println("\n수강생 목록을 조회합니다...");
         for (Student student : studentStore) {
-                System.out.println("아이디 : " + student.getStudentId() + " | 이름 : " + student.getStudentName() + " | 과목 : " + String.join(", ", student.getSubjectList()));
-          }
-            System.out.println("\n수강생 목록 조회 성공!");
+            System.out.println("아이디 : " + student.getStudentId() + " | 이름 : " + student.getStudentName() + " | 과목 : " + String.join(", ", student.getSubjectList()));
+        }
+        System.out.println("\n수강생 목록 조회 성공!");
     }
 
     private static void displayScoreView() {
@@ -291,14 +292,68 @@ public class CampManagementApplication {
 
     private static String getStudentId() {
         System.out.print("\n관리할 수강생의 번호를 입력하시오...");
-        return sc.next();
+        String id = sc.next();
+        if (studentStore.stream().noneMatch((Student s) -> s.getStudentId().equals(id))) {
+            return null;
+        }
+        return id;
     }
 
     // 수강생의 과목별 시험 회차 및 점수 등록
     private static void createScore() {
-        String studentId = getStudentId(); // 관리할 수강생 고유 번호
         System.out.println("시험 점수를 등록합니다...");
-        // 기능 구현
+        String studentId = getStudentId(); // 관리할 수강생 고유 번호
+        if(studentId == null) {
+            System.out.println("존재하지 않는 수강생입니다.");
+            return;
+        }
+        sc.nextLine();
+
+
+        Student student = studentStore.stream().filter((Student s) -> s.getStudentId().equals(studentId)).toList().get(0); // 수강중인 과목 필터링
+        List<Subject> enrolledSubject = student.getEnrolledSubjects();
+        System.out.println("\n다음은 " + student.getStudentName() + " 학생의 수강 과목입니다.");
+        System.out.printf("%-9s%-20s%n", "과목ID", "과목이름");
+        System.out.println("----------------------------");
+        enrolledSubject.forEach(subject -> {
+            System.out.printf("%-10s%-20s%n", subject.getSubjectId(), subject.getSubjectName());
+        });
+        System.out.println();
+        System.out.println("과목의 번호를 입력하시오");
+        String subjectId = sc.nextLine();
+        if(subjectStore.stream().noneMatch((Subject s) -> s.getSubjectId().equals(subjectId))) {
+            System.out.println("존재하지 않는 과목입니다.");
+            return;
+        }
+
+        System.out.println("점수를 입력하시오");
+        int score = sc.nextInt();
+        if(score > 100 || score < 0) {
+            System.out.println("1부터 100 까지의 점수를 입력하세요");
+            return;
+        }
+
+        System.out.println("회차를 입력하시오");
+        int round = sc.nextInt();
+        if(round > 10 || round < 0) {
+            System.out.println("1부터 10까지의 회차를 입력하세요");
+            return;
+        }
+
+        Student resultStudent = studentStore.stream().filter((Student s) -> s.getStudentId().equals(studentId)).toList().get(0); // score객체에 저장할 student 객체 생성
+        Subject resultSubject = subjectStore.stream().filter((Subject s) -> s.getSubjectId().equals(subjectId)).toList().get(0); // score객체에 저장할 subject 객체 생성
+        if(scoreStore.stream().anyMatch((Score s) -> { return
+                s.getStudent().getStudentId().equals(studentId) &&
+                        s.getSubject().getSubjectId().equals(subjectId) &&
+                        s.getRound() == round;}) // 이름, 과목, 회차 셋 모두 검사해서 중복여부 확인
+        ) {
+            System.out.println("중복된 회차가 있습니다.");
+            return;
+        }
+
+        Score scoreObject = new Score(sequence(INDEX_TYPE_SCORE), resultStudent, resultSubject, round, score);
+        scoreStore.add(scoreObject);
+
         System.out.println("\n점수 등록 성공!");
     }
 
