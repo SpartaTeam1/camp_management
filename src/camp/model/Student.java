b2a65859d953b66de16c46213d4f0926f2adafaa
geon <lumineno@naver.com>
2024-05-07 19:43:11 +0900

    클론

--- src/camp/model/Student.java
+++ src/camp/model/Student.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class Student {
     private String studentId;
     private String studentName;
+
+    private List<Subject> enrolledSubjects; // 수강과목 저장할 리스트
+
     private static int NO = 1;
     private final List<String> subjectList = new ArrayList<>();
 
     public Student(String studentName) {
         studentId = "STU" + NO;
         this.studentName = studentName;
+        this.enrolledSubjects = new ArrayList<>(); // 초기화
         ++NO;
     }
 
@@ -22,7 +26,12 @@ public class Student {
     public String getStudentName() {
         return studentName;
     }
+
     public List<String> getSubjectList() {
         return subjectList;
+    }
+
+    public List<Subject> getEnrolledSubjects() { // 수강과목 getter
+        return enrolledSubjects;
     }
 }
