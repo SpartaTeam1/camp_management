a42aaae9afcd65d953c89b249a17b1ed35f247d9
chamcham0707 <cheamin6306@naver.com>
2024-05-08 10:52:53 +0900

    test: create inquirySpecificSubject branch

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -375,6 +375,9 @@ public class CampManagementApplication {
     private static void inquireRoundGradeBySubject() {
         String studentId = getStudentId(); // 관리할 수강생 고유 번호
         // 기능 구현 (조회할 특정 과목)
+
+        System.out.print("조회할 과목의 ID 값을 입력해주세요: ");
+
         System.out.println("회차별 등급을 조회합니다...");
         // 기능 구현
         System.out.println("\n등급 조회 성공!");
