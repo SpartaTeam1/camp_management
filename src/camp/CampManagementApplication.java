a3c6df9562433721e8058ec5ad440a9d5318868f
chamcham0707 <cheamin6306@naver.com>
2024-05-08 16:29:00 +0900

    test: create inquiryStatusAverageGrade branch test

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -303,7 +303,9 @@ public class CampManagementApplication {
             System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
             System.out.println("2. 수강생의 과목별 회차 점수 수정");
             System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-            System.out.println("4. 메인 화면 이동");
+            System.out.println("4. 수강생의 과목별 평균 등급을 조회");
+            System.out.println("5. 특정 상태 수강생들의 필수 과목 평균 등급을 조회");
+            System.out.println("6. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
 
@@ -311,7 +313,8 @@ public class CampManagementApplication {
                 case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
                 case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
                 case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                case 4 -> flag = false; // 메인 화면 이동
+                case 5 -> inquireAverageGradeSpecificStatusStudent(); // 특정 상태 수강생들의 필수 과목 평균 등급 조회
+                case 6 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
@@ -406,7 +409,7 @@ public class CampManagementApplication {
      * @param score: Score 객체
      * @return 등급
      */
-    public static char getGrade(Score score) {
+    private static char getGrade(Score score) {
         String subjectType = score.getSubject().getSubjectType();
         int scoreValue = score.getScore();
         if (Objects.equals(subjectType, SUBJECT_TYPE_MANDATORY)) {
@@ -551,5 +554,9 @@ public class CampManagementApplication {
                         .equals(subjectId)).forEach(s -> {
             System.out.printf("%-10d%-20d%n", s.getRound(), s.getScore());
         });
+    }
+
+    private static void inquireAverageGradeSpecificStatusStudent() {
+
     }
 }
